ef73f4c47654c9a00edcbcf449bfec5c4bc4a9e4
matsyl1 <156823556+matsyl1@users.noreply.github.com>
2025-02-25 18:56:50 +0100

    add name length validation and frontend error msg

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/api/persons/:id', (req, res, next) => {
   .catch(error => next(error))
 })  
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     if(!body.name || !body.number) {
@@ -64,6 +64,7 @@ app.post('/api/persons', (req, res) => {
         res.status(201).json(savedPerson)
       })
     })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
@@ -140,6 +141,8 @@ const errorHandler = (error, req, res, next) => {
 
   if(error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -15,7 +15,12 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+      type: String, 
+      minLength: 3,
+      required: true
+    },
+    // name: String,
     number: String,
 })
 
